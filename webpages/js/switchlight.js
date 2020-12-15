f3baf99cec14736a9fb3668153ac162452de78be
homeofsnaky <75776939+homeofsnaky@users.noreply.github.com>
2020-12-15 16:44:18 +0700

    Add files via upload

---
+++ webpages/js/switchlight.js
@@ -0,0 +1,6 @@
+function myFunction(){
+	document.getElementById('myBulb').src='https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg';
+}
+function my2Function(){
+	document.getElementById('myBulb').src='https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg';
+}
